291c379bf4d3cb52c21afe90b747c25ca491f2e4
Mohit Chaudahry <mohitch.work@gmail.com>
2021-07-26 19:39:16 +0530

    minor twiks and about component initiate
    minor twiks and about component initiate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillComponent } from './skill/skill.component';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     component: HomeComponent,
   },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillComponent },
 ];
 
 @NgModule({
