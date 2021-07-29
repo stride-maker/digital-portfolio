4a5db846bd9a93b906110c964201353594241114
Mohit Chaudahry <mohitch.work@gmail.com>
2021-07-29 22:00:15 +0530

    contact form page completed
    contact form page completed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Contact Information - Mohit Chaudhary');
   }
 
+  ngOnInit(): void {}
 }
