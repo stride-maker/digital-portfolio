01d1a47335eb8b5447c8e8a3c9b108dead9124e3
Mohit Chaudahry <mohitch.work@gmail.com>
2021-07-31 00:06:35 +0530

    fixed the navbar issue
    fixed the navbar issue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,20 +6,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  @ViewChild('navbarToggler')
-  navbarToggler!: ElementRef<any>;
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  navBarTogglerIsVisible() {
-    return this.navbarToggler.nativeElement.offsetParent !== null;
-  }
-
-  collapseNav() {
-    if (this.navBarTogglerIsVisible()) {
-      this.navbarToggler.nativeElement.click();
-    }
-  }
 }
