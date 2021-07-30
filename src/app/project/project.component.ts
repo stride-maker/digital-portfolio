370db53789630422ba3572ea6a3f9a1b93eaf332
Mohit Chaudahry <mohitch.work@gmail.com>
2021-07-30 22:34:33 +0530

    project page complete
    project page complete

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
 })
 export class ProjectComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Personal Project Work - Mohit Chaudahry');
   }
 
+  ngOnInit(): void {}
 }
